f5589494207acbd2e3b902f39945363194485333
SukanyaC <charukarn2000@yahoo.com>
2019-07-10 14:10:38 +0700

    UI register Success

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -6,13 +6,112 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+// Explicit
+
+// Method
+  Widget uploadButton() {
+    return IconButton(
+      icon: Icon(Icons.cloud_upload),
+      onPressed: () {},
+    );
+  }
+
+  Widget nameText() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Name :',
+        labelStyle: TextStyle(
+          color: Colors.cyan,
+          fontSize: 24.0,
+          fontWeight: FontWeight.bold,
+        ),
+        helperText: 'FirstName and LastName',
+        helperStyle: TextStyle(
+          color: Colors.cyan,
+          fontSize: 14.0,
+          fontStyle: FontStyle.italic,
+        ),
+        icon: Icon(
+          Icons.account_circle,
+          size: 36.0,
+          color: Colors.cyan,
+        ),
+      ),
+    );
+  }
+
+  Widget emailText() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Email :',
+        labelStyle: TextStyle(
+          color: Colors.lime[900],
+          fontSize: 24.0,
+          fontWeight: FontWeight.bold,
+        ),
+        helperText: 'Email Format',
+        helperStyle: TextStyle(
+          color: Colors.lime[900],
+          fontSize: 14.0,
+          fontStyle: FontStyle.italic,
+        ),
+        icon: Icon(
+          Icons.email,
+          size: 36.0,
+          color: Colors.lime,
+        ),
+      ),
+    );
+  }
+
+  Widget passwordText() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Password :',
+        labelStyle: TextStyle(
+          color: Colors.deepOrange,
+          fontSize: 24.0,
+          fontWeight: FontWeight.bold,
+        ),
+        helperText: 'More 6 Charactor',
+        helperStyle: TextStyle(
+          color: Colors.deepOrange,
+          fontSize: 14.0,
+          fontStyle: FontStyle.italic,
+        ),
+        icon: Icon(
+          Icons.lock,
+          size: 36.0,
+          color: Colors.deepOrange,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       appBar: AppBar(
+        actions: <Widget>[uploadButton()],
+        backgroundColor: Colors.teal[300],
         title: Text('Register'),
-      ),body: Text('body'),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(top: 40.0),
+        alignment: Alignment.topCenter,
+        child: Container(
+          width: 240.0,
+          child: Column(
+            children: <Widget>[
+              nameText(),
+              emailText(),
+              passwordText(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
-
